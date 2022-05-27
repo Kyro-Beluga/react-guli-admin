7dd873ee6b98fdb0953efe9257ff784757f7a461
Beluga <1511397550@qq.com>
2022-05-27 21:20:02 +0800

    Router 6 坑太多，先修复再弄后面的内容

---
+++ src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+/*
+    Admin右侧顶部
+ */
+class Header extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default Header;
